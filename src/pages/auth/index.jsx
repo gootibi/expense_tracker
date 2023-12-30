1878902726326d4aa222bd0c9512ed164636a060
gootibi <gootibi@gmail.com>
2023-12-31 00:49:38 +0100

    Clear bug Changes to be committed: 	new file:   .firebase/hosting.YnVpbGQ.cache 	new file:   .firebaserc 	new file:   .github/workflows/firebase-hosting-merge.yml 	new file:   .github/workflows/firebase-hosting-pull-request.yml 	deleted:    firebase-debug.log 	new file:   firebase.json 	modified:   src/hooks/useGetUserInfo.js 	modified:   src/pages/auth/index.jsx

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,14 +1,15 @@
 import { auth, provider } from '../../config/firebase-config'
 import { signInWithPopup } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate, Navigate } from 'react-router-dom'
+import { useGetUserInfo } from '../../hooks/useGetUserInfo'
 import './styles.css'
 
 
 export const Auth = () => {
-;
+  ;
   const navigate = useNavigate();
-  
+  const { isAuth } = useGetUserInfo();
+
   const signInWithGoogle = async () => {
     try {
       const results = await signInWithPopup(auth, provider);
@@ -25,14 +26,14 @@ export const Auth = () => {
     }
   };
 
+  if (isAuth) {
+    return <Navigate to="/expense-tracker" />;
+  }
+
   return (
     <div className="login-page">
       <p>Sign in With Google to Continue</p>
       <button className="login-with-google-btn" onClick={signInWithGoogle}>Sign in With Google</button>
     </div>
   )
-
-
-
-
 }
